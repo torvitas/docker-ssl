9b82140781c49b888606b698aa19dd974be89c9c
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2016-03-18 16:03:53 +0100

    updated configuration

--- bin/entrypoint.environment.sh
+++ bin/entrypoint.environment.sh
@@ -2,28 +2,28 @@
 
 export UNIQUE_CERT_NAME=${UNIQUE_CERT_NAME:-}
 
-export CN=${CN:-'*.localhost'}
-export OU=${OU:-'A Company that Makes Everything'}
+export CN=${CN:-'A Company that Makes Everything'}
 export O=${O:-'ACME'}
+export OU=${OU:-'Domain Control Validated'}
 
-export PROXY_CERTS=${PROXY_CERTS:-'/proxy/certs/'}
-export CERTS_FOLDER=${CERTS_FOLDER:-'/usr/local/etc/ssl/certs/'}
-export CA_FOLDER=${CA_FOLDER:-'/usr/local/etc/ssl/ca/'}
+export PROXY_CERTS=${PROXY_CERTS:-'/proxy/certs'}
+export CERTS_FOLDER=${CERTS_FOLDER:-'/usr/local/etc/ssl/certs'}
+export CA_FOLDER=${CA_FOLDER:-'/usr/local/etc/ssl/ca'}
 
-export CA_KEY_FILE=${CA_KEY_FILE:-'root.key'}
-export CA_CRT_FILE=${CA_CRT_FILE:-'root.crt'}
+export CA_KEY_FILE=${CA_KEY_FILE:-'cakey.pem'}
+export CA_CRT_FILE=${CA_CRT_FILE:-'cacrt.pem'}
 
-export CA_KEY=${CA_KEY:-${CA_FOLDER}${CA_KEY_FILE}}
-export CA_CRT=${CA_CRT:-${CA_FOLDER}${CA_CRT_FILE}}
+export CA_KEY=${CA_KEY:-${CA_FOLDER}'/'${CA_KEY_FILE}}
+export CA_CRT=${CA_CRT:-${CA_FOLDER}'/'${CA_CRT_FILE}}
 
-export CA_CN=${CA_CN:-'*.localhost'}
-export CA_OU=${CA_OU:-'A Company that Makes Everything'}
-export CA_O=${CA_O:-'ACME'}
+export CA_CN=${CA_CN:-'A Company that Validates Everything'}
+export CA_O=${CA_O:-'ACVE'}
+export CA_OU=${CA_OU:-'Validations for everything'}
 
-export KEY_FILE=${KEY_FILE:-${UNIQUE_CERT_NAME}'.key'}
+export KEY_FILE=${KEY_FILE:-${UNIQUE_CERT_NAME}'key.pem'}
+export CRT_FILE=${CRT_FILE:-${UNIQUE_CERT_NAME}'crt.pem'}
 export CSR_FILE=${CSR_FILE:-${UNIQUE_CERT_NAME}'.csr'}
-export CRT_FILE=${CRT_FILE:-${UNIQUE_CERT_NAME}'.crt'}
 
-export KEY=${KEY:-${CERTS_FOLDER}${KEY_FILE}}
-export CSR=${CSR:-${CERTS_FOLDER}${CSR_FILE}}
-export CRT=${CRT:-${CERTS_FOLDER}${CRT_FILE}}
+export KEY=${KEY:-${CERTS_FOLDER}'/'${KEY_FILE}}
+export CSR=${CSR:-${CERTS_FOLDER}'/'${CSR_FILE}}
+export CRT=${CRT:-${CERTS_FOLDER}'/'${CRT_FILE}}
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -7,32 +7,49 @@
 
 # make sure there the specified ca exists
 if [ ! -f ${CA_KEY} ]; then
-    openssl genrsa -out ${CA_KEY} 2048
-    render /usr/local/etc/ssl/template/root.cnf.template -- > /usr/local/etc/ssl/rootssl.conf
-    openssl req -x509 -new -nodes -key ${CA_KEY} -out ${CA_CRT} -config /usr/local/etc/ssl/rootssl.conf
+    cd ${CA_FOLDER}
+    echo '>> Rendering ca config template.'
+    render /usr/local/etc/ssl/template/ca.cnf.template -- > /usr/local/etc/ssl/ca.cnf
+    touch ${CA_FOLDER}'/index.txt'
+    echo '01' > ${CA_FOLDER}'/serial.txt'
+    echo '>> Creating ca certificate and key.'
+    openssl req -x509 -config /usr/local/etc/ssl/ca.cnf -newkey rsa:4096 -sha256 -nodes -out ${CA_CRT_FILE} -outform PEM
+    openssl x509 -in ${CA_CRT_FILE} -text -noout
+    ls -lah ${CA_FOLDER}
 else
     echo '>> Using existing CA.'
 fi
 
+# create cert if necessary
 if [ ! -f ${KEY} ]; then
-    openssl genrsa -out ${KEY} 2048
-    render /usr/local/etc/ssl/template/host.cnf.template -- > /usr/local/etc/ssl/hostssl.conf
-    openssl req -new -nodes -key ${KEY} -out ${CSR} -config /usr/local/etc/ssl/hostssl.conf
-    openssl x509 -req -in ${CSR} -CA ${CA_CRT} -CAkey ${CA_KEY} -CAcreateserial -out ${CRT} -days 365
+    cd ${CERTS_FOLDER}
+    host_setup=1
+    echo '>> Rendering host config template.'
+    render /usr/local/etc/ssl/template/host.cnf.template -- > /usr/local/etc/ssl/host.cnf
+    cat /usr/local/etc/ssl/host.cnf
+    echo '>> Creating certificate signing request.'
+    openssl req -config /usr/local/etc/ssl/host.cnf -newkey rsa:2048 -sha256 -nodes -out ${CSR_FILE} -outform PEM
+    openssl req -text -noout -verify -in ${CSR_FILE}
+    echo '>> Signing certificate using ca in '${CA_FOLDER}
+    openssl ca -batch -config /usr/local/etc/ssl/ca.cnf -policy signing_policy -extensions signing_req -out ${CRT_FILE} -infiles ${CSR_FILE}
 else
     echo '>> Using existing certificate, doing nothing.'
 fi
 
+# publish cert to nginx proxy if necessary
 if [ ! -f ${PROXY_CERTS}${KEY_FILE} ]; then
-    cp ${KEY} ${PROXY_CERTS}
-    cp ${CRT} ${PROXY_CERTS}
+    mkdir -p ${PROXY_CERTS}
+    cp -v ${KEY} ${PROXY_CERTS}
+    cp -v ${CRT} ${PROXY_CERTS}
 else
-    echo '>> Certificate already available for proxy, doing nothing.'
+    echo '>> Using existing proxy certificate, doing nothing.'
 fi
 
-echo ">> CSR:"
-openssl req -in ${CSR} -noout -text
-echo ">> CRT:"
-cat ${CRT}
+if [ ! host_setup ]; then
+    echo ">> CSR:"
+    openssl req -in ${CSR} -noout -text
+    echo ">> CRT:"
+    cat ${CRT}
+fi
 
 exec "$@"
